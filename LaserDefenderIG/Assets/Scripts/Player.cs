a2950ae0e5f50480704c494de2a08b41401dd3f2
Issy19095 <73161801+Issy19095@users.noreply.github.com>
2020-11-03 11:00:24 +0100

    Border and MovementSpeed

--- LaserDefenderIG/Assets/Scripts/Player.cs
+++ LaserDefenderIG/Assets/Scripts/Player.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{ 
+    //this make it editable from unity
+    [SerializeField] float moveSpeed = 30f;
+    [SerializeField] float padding = 0.7f;
+
+    float xMin, xMax, yMin, yMax;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        //get the main camera from Unity
+        Camera gameCamera = Camera.main;
+        //set boundaries on the x-axis
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
         
+        //set boundaries on the y-axis
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -21,16 +41,20 @@ public class Player : MonoBehaviour
     {
         //var changes its variable type according on what i save in it
         //deltaX will have the difference in the x-axis which the Player moves
-        var deltaX = Input.GetAxis("Horizontal");
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         //newXPos = current x-pos + difference in x
         var newXPos = transform.position.x + deltaX;
+
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
 
         
 
 
         //to move on the y-axis:
-        var deltaY = Input.GetAxis("Vertical");
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         var newYPos = transform.position.y + deltaY;
+
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
 
         this.transform.position = new Vector2(newXPos, newYPos);
     }
