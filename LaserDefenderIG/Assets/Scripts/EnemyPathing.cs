732fb95b562ff35b1dbea0bbc00b17c2f8674dba
Issy19095 <73161801+Issy19095@users.noreply.github.com>
2020-11-11 14:12:26 +0100

    Enemy movement
    We created a path for the enemy to follow while the game is running. at the end the enemy will be destroyed.

--- LaserDefenderIG/Assets/Scripts/EnemyPathing.cs
+++ LaserDefenderIG/Assets/Scripts/EnemyPathing.cs
@@ -5,18 +5,46 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    //to show the next waypoint
+    int waypointIndex = 0;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set the startin pos of ship at waypoint 0
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMove();
+    }
+
+    private void EnemyMove()
+    {
+        if(waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            targetPosition.z = 0f;
+            var enemyMovement = enemyMoveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, enemyMovement);
+            
+            //if target waypoint is reached upadted to next waypoint
+            if(transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
