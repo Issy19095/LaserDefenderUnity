f23c2cecc9f968429971f9ec14fc911ab71cf032
Issy19095 <73161801+Issy19095@users.noreply.github.com>
2020-11-16 19:20:42 +0100

    Added 2nd Enemy with path

---
+++ LaserDefenderIG/Assets/Scripts/EnemyPathing2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing2 : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    int waypointIndex = 0;
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    private void EnemyMove()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            targetPosition.z = 0f;
+            var enemyMovement = enemyMoveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, enemyMovement);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
